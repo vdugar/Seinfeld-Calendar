928ab5de46ceed28dc34449c212ad0a66fbe78b4
Vishal Dugar <vishaldugar12@gmail.com>
2011-05-29 17:43:44 +0530

    activated admin

--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('seincal.views',
     (r'^$','landing'),
 	(r'^get_streaks/$','get_streaks'),
 	(r'^post_streak/$','post_streak'),
+	(r'^admin/', include(admin.site.urls)),
 )
